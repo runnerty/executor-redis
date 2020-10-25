e4245eee39071bf9733e1e58d90d1d270954d491
carlos <carloscarmonaalcantara@gmail.com>
2020-10-25 12:25:42 +0100

    build: 2.0.0

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
+<p align="center">
+  <a href="http://runnerty.io">
+    <img height="257" src="https://runnerty.io/assets/header/logo-stroked.png">
+  </a>
+  <p align="center">Smart Processes Management</p>
+</p>
+
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency Status][david-badge]][david-badge-url]
+<a href="#badge">
+  <img alt="code style: prettier" src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg">
+</a>
+
 # Redis executor for [Runnerty]:
+
+### Installation:
+```bash
+npm i @runnerty/executor-redis
+```
 
 ### Configuration sample:
 ```json
@@ -8,7 +25,7 @@
   "password": "redis_password",
   "host": "redishost.com",
   "port": "6379",
-  "db": "0",
+  "db": 0,
   "options": {}
 }
 ```
@@ -33,3 +50,10 @@
 * `PROCESS_EXEC_ERR_OUTPUT`: Error output message.
 
 [Runnerty]: http://www.runnerty.io
+[downloads-image]: https://img.shields.io/npm/dm/@runnerty/executor-redis.svg
+[npm-url]: https://www.npmjs.com/package/@runnerty/executor-redis
+[npm-image]: https://img.shields.io/npm/v/@runnerty/executor-redis.svg
+[david-badge]: https://david-dm.org/runnerty/executor-redis.svg
+[david-badge-url]: https://david-dm.org/runnerty/executor-redis
+[config.json]: http://docs.runnerty.io/config/
+[plan.json]: http://docs.runnerty.io/plan/
--- index.js
+++ index.js
@@ -1,110 +1,104 @@
-"use strict";
+'use strict';
 
-var redis = require("redis");
-var loadFile = global.libUtils.loadSQLFile;
-
-var Execution = global.ExecutionClass;
+const redis = require('redis');
+const loadFile = global.libUtils.loadSQLFile;
+const Execution = global.ExecutionClass;
 
 class redisExecutor extends Execution {
   constructor(process) {
     super(process);
   }
 
-  exec(res) {
-    var _this = this;
-    var endOptions = {end: "end"};
+  async exec(res) {
+    const self = this;
+    const endOptions = { end: 'end' };
 
     function commandsFormat(command) {
-      let res = [];
-      let lines = command.split(/\r\n|\n/);
+      const res = [];
+      const lines = command.split(/\r\n|\n/);
       for (let i = 0; i < lines.length; i++) {
-        res.push(lines[i].trim().split(" "));
+        res.push(lines[i].trim().split(' '));
       }
       return res;
     }
 
-    function executeCommand(configValues) {
-      return new Promise(async function (resolve, reject) {
-
-        var options = {
+    async function executeCommand(configValues) {
+      return new Promise(async (resolve, reject) => {
+        const options = {
           useExtraValue: configValues.args || false,
           useProcessValues: true,
           useGlobalValues: true,
-          altValueReplace: "null"
+          altValueReplace: 'null'
         };
 
-        if(configValues.command_file){
-          await loadFile(configValues.command_file)
-            .then((fileContent) => {
-              res.command = fileContent;
-            })
-            .catch(function (err) {
-              _this.logger.log("error", "Loading redis command file: " + err);
-              reject("Loading redis command file: " + err);
-            });
+        if (configValues.command_file) {
+          try {
+            const fileContent = await loadFile(configValues.command_file);
+            res.command = fileContent;
+          } catch (err) {
+            this.logger.log('error', 'Loading redis command file: ' + err);
+            reject('Loading redis command file: ' + err);
+          }
         }
 
-        var _query = await _this.paramsReplace(res.command, options);
-        
-        if(configValues.password && configValues.password !== ""){
-          configValues.options.password = configValues.password || "";
+        const _query = await self.paramsReplace(res.command, options);
+
+        if (configValues.password && configValues.password !== '') {
+          configValues.options.password = configValues.password || '';
         }
         configValues.options.db = configValues.db || 0;
 
-        var redisClient = redis.createClient(configValues.port || "6379", configValues.host, configValues.options);
+        const redisClient = redis.createClient(configValues.port || '6379', configValues.host, configValues.options);
 
-        redisClient.on("error", function (err) {
-          _this.logger.log("error", "Could not connect to Redis: " + err);
-          reject("Could not connect to Redis: " + err);
+        redisClient.on('error', err => {
+          this.logger.log('error', 'Could not connect to Redis: ' + err);
+          reject('Could not connect to Redis: ' + err);
         });
 
-        redisClient.on("ready", function () {
-          var commands = commandsFormat(_query);
+        redisClient.on('ready', () => {
+          const commands = commandsFormat(_query);
           endOptions.command_executed = commands;
-
           try {
-            redisClient
-              .batch(commands)
-              .exec(function (err, replies) {
-                redisClient.quit();
-                if (err) {
-                  _this.logger.log("error", `Error query Redis (${commands}): ` + err);
-                  reject(`Error query Redis (${commands}): ` + err);
-                } else {
-                  resolve(replies);
-                }
-              });
+            redisClient.batch(commands).exec((err, replies) => {
+              redisClient.quit();
+              if (err) {
+                this.logger.log('error', `Error query Redis (${commands}): ` + err);
+                reject(`Error query Redis (${commands}): ` + err);
+              } else {
+                resolve(replies);
+              }
+            });
           } catch (err) {
-            _this.logger.log("error", "Error query Redis, check commands: " + commands, err);
-            reject("Error query Redis, check commands: " + commands, err);
+            this.logger.log('error', 'Error query Redis, check commands: ' + commands, err);
+            reject('Error query Redis, check commands: ' + commands, err);
           }
         });
       });
     }
 
     if (res.command || res.command_file) {
-      executeCommand(res)
-        .then((res) => {
-          // STANDARD OUTPUT
-          endOptions.end = "end";
-          endOptions.data_output = res;
-          // EXTRA DATA OUTPUT
-          endOptions.extra_output = {};
-          endOptions.extra_output.db_firstRow = JSON.stringify(res[0]);
-
-          _this.end(endOptions);
-        })
-        .catch(function (err) {
-          endOptions.end = "error";
-          endOptions.messageLog = `executeRedis executeCommand: ${err}`;
-          endOptions.err_output = `executeRedis executeCommand: ${err}`;
-          _this.end(endOptions);
-        });
+      try {
+        const response = await executeCommand(res);
+        // STANDARD OUTPUT
+        endOptions.end = 'end';
+        endOptions.data_output = response;
+        // EXTRA DATA OUTPUT
+        endOptions.extra_output = {};
+        if (response && response[0]) {
+          endOptions.extra_output.db_firstRow = JSON.stringify(response[0]);
+        }
+        this.end(endOptions);
+      } catch (err) {
+        endOptions.end = 'error';
+        endOptions.messageLog = `executeRedis executeCommand: ${err}`;
+        endOptions.err_output = `executeRedis executeCommand: ${err}`;
+        this.end(endOptions);
+      }
     } else {
-      endOptions.end = "error";
-      endOptions.messageLog = `executeRedis: command not set and command_file nor supported yet for ${_this.processId}(${_this.processUId}.`;
-      endOptions.err_output = `executeRedis: command not set and command_file nor supported yet for ${_this.processId}(${_this.processUId}.`;
-      _this.end(endOptions);
+      endOptions.end = 'error';
+      endOptions.messageLog = `executeRedis: command not set and command_file nor supported yet for ${this.processId}(${this.processUId}.`;
+      endOptions.err_output = `executeRedis: command not set and command_file nor supported yet for ${this.processId}(${this.processUId}.`;
+      this.end(endOptions);
     }
   }
 }
